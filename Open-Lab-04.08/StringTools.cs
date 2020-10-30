1cf05966d17937da568819c2900978be89944f03
Martin Kubica <martinko29.km@gmail.com>
2020-10-30 15:06:38 +0100

    +

--- Open-Lab-04.08/StringTools.cs
+++ Open-Lab-04.08/StringTools.cs
@@ -1,12 +1,23 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace Open_Lab_04._08
 {
     public class StringTools
     {
         public string[] IsFourLetters(string[] strings)
         {
-            throw new NotImplementedException();
+            List<string> q = new List<string>();
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                if (strings[i].Length == 4) 
+                {
+                    q.Add(strings[i]);
+                    
+                }
+                    
+            }
+            return q.ToArray();
         }
     }
 }
